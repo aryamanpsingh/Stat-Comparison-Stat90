acdb60b72fd159c45531d311a365d0b529998eb5
Aryaman Singh <aps010@ucsd.edu>
2019-09-25 01:14:28 -0700

    updated update.py

--- update.py
+++ update.py
@@ -5,6 +5,65 @@ from graphdata.models import Player
 # Serie A
 csvInfo = [
     {
+        'name': "premierleague2019.csv",
+        'year': 2019,
+        'league': 1
+    },
+    {
+        'name': "premierleague2018.csv",
+        'year': 2018,
+        'league': 1
+    },
+    {
+        'name': "laliga2019.csv",
+        'year': 2019,
+        'league': 2
+    },
+    {
+        'name': "laliga2018.csv",
+        'year': 2018,
+        'league': 2
+    }
+]
+for season in csvInfo:
+    try:
+        Player.objects.filter(
+            league=season["league"], year=season["year"]).delete()
+    except:
+        Player.objects.filter(
+            league=season["league"], year=season["Year"]).delete()
+'''
+    with open(season["name"]) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            p = Player(name=row['name'], team=row['team'], apps=row['apps'], mins=row['mins'], goals=row['goals'], assists=row['assists'],
+                       xG=row['xG'], xA=row['xA'], xG90=row['xG90'], xA90=row['xG90'], year=row['year'], league=row['league'])
+            p.save()
+'''
+'''
+    {
+        'name': "premierleague2019.csv",
+        'year': 2019,
+        'league': 1
+    },
+    {
+        'name': "premierleague2018.csv",
+        'year': 2018,
+        'league': 1
+    },
+    {
+        'name': "laliga2019.csv",
+        'year': 2019,
+        'league': 2
+    },
+    {
+        'name': "laliga2018.csv",
+        'year': 2018,
+        'league': 2
+    },
+    '''
+'''
+    {
 
         'name': "serieA2019.csv",
         'year': 2019,
@@ -35,42 +94,4 @@ csvInfo = [
         'year': 2019,
         'league': 5
     }
-
-]
-for season in csvInfo:
-    try:
-        Player.objects.filter(
-            league=season["league"], year=season["year"]).delete()
-    except:
-        Player.objects.filter(
-            league=season["league"], year=season["Year"]).delete()
-
-    with open(season["name"]) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            p = Player(name=row['name'], team=row['team'], apps=row['apps'], mins=row['mins'], goals=row['goals'], assists=row['assists'],
-                       xG=row['xG'], xA=row['xA'], xG90=row['xG90'], xA90=row['xG90'], year=row['year'], league=row['league'])
-            p.save()
-
-    '''
-    {
-        'name': "premierleague2019.csv",
-        'year': 2019,
-        'league': 1
-    },
-    {
-        'name': "premierleague2018.csv",
-        'year': 2018,
-        'league': 1
-    },
-    {
-        'name': "laliga2019.csv",
-        'year': 2019,
-        'league': 2
-    },
-    {
-        'name': "laliga2018.csv",
-        'year': 2018,
-        'league': 2
-    },
-    '''
+'''

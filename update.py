236fa78641d6142fce522da5930edff59455e712
Aryaman Singh <aps010@ucsd.edu>
2019-09-25 01:37:28 -0700

    updated update.py

--- update.py
+++ update.py
@@ -22,14 +22,14 @@ for season in csvInfo:
     except:
         Player.objects.filter(
             league=season["league"], year=season["Year"]).delete()
-        '''
+
     with open(season["name"]) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             p = Player(name=row['name'], team=row['team'], apps=row['apps'], mins=row['mins'], goals=row['goals'], assists=row['assists'],
                        xG=row['xG'], xA=row['xA'], xG90=row['xG90'], xA90=row['xG90'], year=row['year'], league=row['league'])
             p.save()
-            '''
+
 '''
     {
         'name': "premierleague2019.csv",
